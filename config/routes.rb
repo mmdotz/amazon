6f644fc976f15f29d32b2fff55e68b5d5ff111ec
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-10-14 18:03:55 -0500

    create sessions controller, style buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/index'
+
+  get 'sessions/show'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   resources :orders
   resources :items
   resources :users
