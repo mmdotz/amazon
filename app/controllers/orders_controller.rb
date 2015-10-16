13c38ac8ad39494f85b7b511351317de097a6376
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-10-16 11:29:33 -0500

    show order detail

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
   # GET /orders/1
   # GET /orders/1.json
   def show
+    @lineitem = Lineitem.new
   end
 
   # GET /orders/new
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ActiveRecord::Base
   belongs_to :user
   has_many :lineitems, dependent: :destroy
+  has_many :items, through: :lineitems
 end
